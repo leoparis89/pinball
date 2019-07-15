3c4821ab542dc3eb7c909d4b3c10b869df038819
Lev Kowalski <levkowalski@gmail.com>
2019-07-15 10:47:01 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,165 +1,72 @@
-import Phaser from 'phaser'
+import * as Matter from 'matter-js'
 
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 1200,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
+const { Engine, Render, World, Bodies } = Matter
+
+const canvasH = 1000
+const canvasW = 800
+
+// create an engine
+const engine = Engine.create()
+
+// create a renderer
+const render = Render.create({
+  element: document.body,
+  engine,
+  options: {
+    height: canvasH,
+    width: canvasW,
+    // wireframes: false,
   },
-  scene: {
-    preload,
-    create,
-    update,
-  },
-}
+})
 
-const game = new Phaser.Game(config)
+// Ground
+const groundW = 20
+const ground = Bodies.rectangle(
+  canvasW / 2,
+  canvasH - groundW / 2,
+  canvasW,
+  groundW,
+  { isStatic: true },
+)
 
-function preload() {
-  this.load.image('sky', 'assets/sky.png')
-  this.load.image('ground', 'assets/platform.png')
-  this.load.image('star', 'assets/star.png')
-  this.load.image('bomb', 'assets/bomb.png')
-  this.load.spritesheet('dude', 'assets/dude.png', {
-    frameWidth: 32,
-    frameHeight: 48,
-  })
-}
+// Walls
+const wallW = 20
+// const leftWall = Bodies.rectangle(wallW / 2, canvasH / 2, wallW, canvasH, {
+//   isStatic: true,
+// })
+// const rightWall = Bodies.rectangle(
+//   canvasW - wallW / 2,
+//   canvasH / 2,
+//   wallW,
+//   canvasH,
+//   { isStatic: true },
+// )
 
-let platforms
-let player
-let cursors
-let stars
-let bombs
-const score = 0
-let scoreText
-const gameOver = false
+// Spikes
+const step = 80
+const spikeH = 90
+const spikeW = 10
+const spikes = []
 
-function create() {
-  const scene: Phaser.Scene = this
-  const ball = scene.add.circle(200, 200, 20, 0x6666ff)
-  const floor = scene.add.rectangle(400, 800, 600, 20, 0x6666ff)
-
-  const b = scene.physics.add.staticGroup()
-  b.add(floor)
-  scene.physics.add.existing(ball)
-  scene.physics.add.collider(ball, floor)
-  // const circle = new Phaser.GameObjects.Shape(scene, 'star')
-  // scene.add.image(400, 300, 'sky')
-  // const circle = new Phaser.GameObjects.Shape(scene, 'star')
-  // scene.physics.add.existing(circle)
-  // this.add.image(400, 300, 'star')
-  // platforms = this.physics.add.staticGroup()
-
-  // platforms
-  //   .create(400, 568, 'ground')
-  //   .setScale(2)
-  //   .refreshBody()
-
-  // platforms.create(600, 400, 'ground')
-  // platforms.create(50, 250, 'ground')
-  // platforms.create(750, 220, 'ground')
-
-  // scene.physics.add.sprite(300, 450, 'dude')
-  // player = this.physics.add.sprite(300, 450, 'dude')
-
-  // const scene: Phaser.Scene = this
-  // const player2 = new Phaser.GameObjects.Sprite(scene, 200, 450, 'dude')
-  // const circle = new Phaser.GameObjects.Shape(scene, 'star')
-  // player.setBounce(0.2)
-  // this.physics.add.collider(player, platforms)
-
-  // this.anims.create({
-  //   key: 'left',
-  //   frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-  //   frameRate: 10,
-  //   repeat: -1,
-  // })
-
-  // this.anims.create({
-  //   key: 'turn',
-  //   frames: [{ key: 'dude', frame: 4 }],
-  //   frameRate: 20,
-  // })
-
-  // this.anims.create({
-  //   key: 'right',
-  //   frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-  //   frameRate: 10,
-  //   repeat: -1,
-  // })
-  // cursors = this.input.keyboard.createCursorKeys()
-
-  // stars = this.physics.add.group({
-  //   key: 'star',
-  //   repeat: 11,
-  //   setXY: { x: 12, y: 0, stepX: 70 },
-  // })
-  // stars.children.iterate(child => {
-  //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
-  // })
-  // this.physics.add.collider(stars, platforms)
-  // // this.physics.add.overlap(player, stars, collectStar, null, this)
-
-  // scoreText = this.add.text(16, 16, 'score: 0', {
-  //   fontSize: '32px',
-  //   fill: '#000',
-  // })
-  // bombs = this.physics.add.group()
-
-  // this.physics.add.collider(bombs, platforms)
-
-  // this.physics.add.collider(player, bombs, hitBomb, null, this)
-}
-function update() {
-  // if (cursors.left.isDown) {
-  //   player.setVelocityX(-160)
-  //   player.anims.play('left', true)
-  // } else if (cursors.right.isDown) {
-  //   player.setVelocityX(160)
-  //   player.anims.play('right', true)
-  // } else {
-  //   player.setVelocityX(0)
-  //   player.anims.play('turn')
-  // }
-  // if (cursors.up.isDown && player.body.touching.down) {
-  //   player.setVelocityY(-330)
-  // }
-}
-
-// function collectStar(player, star) {
-//   star.disableBody(true, true)
-
-//   score += 10
-//   scoreText.setText('Score: ' + score)
-
-//   if (stars.countActive(true) === 0) {
-//     stars.children.iterate(child => {
-//       child.enableBody(true, child.x, 0, true, true)
-//     })
-
-//     const x =
-//       player.x < 400
-//         ? Phaser.Math.Between(400, 800)
-//         : Phaser.Math.Between(0, 400)
-
-//     const bomb = bombs.create(x, 16, 'bomb')
-//     bomb.setBounce(1)
-//     bomb.setCollideWorldBounds(true)
-//     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
-//   }
+const leftWall = Bodies.rectangle(10, 30, 50, 30)
+// const circle = Bodies.circle()
+// for (let i = step; i < canvasW; i += step) {
+//   console.log(i)
+//   spikes.push(
+//     Bodies.rectangle(
+//       i - spikeW / 2,
+//       canvasH - spikeH / 2 - groundW,
+//       spikeW,
+//       spikeH,
+//       { isStatic: true },
+//     ),
+//   )
 // }
-// function hitBomb(player, bombs) {
-//   this.physics.pause()
 
-//   player.setTint(0xff0000)
+World.add(engine.world, [ground, leftWall])
 
-//   player.anims.play('turn')
+// run the engine
+Engine.run(engine)
 
-//   gameOver = true
-// }
+// run the renderer
+Render.run(render)
