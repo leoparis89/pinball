19ff2e25c4d8f2d741691ebc8117ae2ed8555350
Lev Kowalski <levkowalski@gmail.com>
2019-07-16 13:30:32 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as PIXI from 'pixi.js'
 import * as Matter from 'matter-js'
 const { Engine, Render, World, Bodies } = Matter
 
-const engine = Engine.create()
+const engine = Engine.create({})
 const world = engine.world
 // const box = Bodies.rectangle(50, 50, 80, 80)
 // console.log(box)
@@ -23,28 +23,62 @@ document.body.appendChild(app.view)
 // app.stage.addChild(circle)
 
 const loader = new PIXI.Loader()
+
+loader
+  .add(['assets/aquaBall.png', 'assets/bomb.png', 'assets/cat.png'])
+  .on('progress', loadProgressHandler)
+// .load(setup)
+
 loader.load(() => {
   app.ticker.add(delta => gameLoop(delta))
+  const cat = new PIXI.Sprite(loader.resources['assets/cat.png'].texture)
+  cat.x = 96
+  cat.y = 96
+
+  const rectangle = new PIXI.Graphics()
+  rectangle.lineStyle(4, 0xff3300, 1)
+  rectangle.beginFill(0x66ccff)
+  rectangle.drawRect(0, 0, 64, 64)
+  rectangle.endFill()
+  rectangle.x = 170
+  rectangle.y = 170
+  app.stage.addChild(rectangle)
+  // Add the cat to the stage
+  app.stage.addChild(cat)
+  // cat.visible = false
+  console.log(app)
 })
 
-class MyBox {
-  body
-  image
-  constructor(x, y, w, h) {
-    this.body = Bodies.rectangle(x, y, w, h)
-    World.add(world, this.body)
-    this.image = new PIXI.Graphics()
-    this.image.beginFill(0x9966ff)
-    this.image.drawRect(x, y, w, h)
-    this.image.endFill()
-    app.stage.addChild(this.image)
-  }
+function loadProgressHandler(loader, resource) {
+  // Display the file `url` currently being loaded
+  console.log('loading: ' + resource.url)
 
-  show() {
-    this.image.x = this.body.position.x
-    this.image.y = this.body.position.y
-  }
+  // Display the percentage of files currently loaded
+  console.log('progress: ' + loader.progress + '%')
+
+  // If you gave your files names as the first argument
+  // of the `add` method, you can access them like this
+  // console.log("loading: " + resource.name);
 }
+
+// class MyBox {
+//   body
+//   image
+//   constructor(x, y, w, h) {
+//     this.body = Bodies.rectangle(x, y, w, h)
+//     World.add(world, this.body)
+//     this.image = new PIXI.Graphics()
+//     this.image.beginFill(0x9966ff)
+//     this.image.drawRect(x, y, w, h)
+//     this.image.endFill()
+//     app.stage.addChild(this.image)
+//   }
+
+//   show() {
+//     this.image.x = this.body.position.x
+//     this.image.y = this.body.position.y
+//   }
+// }
 
 // const canvasH = 1000
 // const canvasW = 800
@@ -117,15 +151,12 @@ class MyBox {
 // Boxes.push(bar)
 
 function gameLoop(delta) {
-  // circle.x = box.position.x
-  // circle.y = box.position.y
-  Boxes.forEach(b => b.show())
+  Boxes.forEach(b => b.refresh())
 }
 
 // window.onclick = e => {
 //   const newB = new MyBox(0, 0, 30, 30)
-//   Boxes.push(newB)
-// }
+//   Boxes.push(newB) // }
 
 // window.document.querySelector('canvas').addEventListener('onClick', e => {
 //   debugger
@@ -134,8 +165,8 @@ function gameLoop(delta) {
 window.document.querySelector('canvas')!.addEventListener('click', e => {
   // const newB = new MyBox(390, 200, 30, 30)
   // Boxes.push(newB)
-
-  app.stage.addChild(makeImage(e.x, e.y))
+  Boxes.push(new Box(e.x, e.y))
+  // app.stage.addChild(makeImage(e.x, e.y))
 })
 
 const makeImage = (x, y) => {
@@ -143,7 +174,34 @@ const makeImage = (x, y) => {
   image.beginFill(0x9966ff)
   image.drawRect(x, y, 40, 40)
   image.endFill()
+  // const image = new PIXI.Sprite(loader.resources['assets/cat.png'].texture)
+  app.stage.addChild(image)
   return image
+}
+
+//     this.body = Bodies.rectangle(x, y, w, h)
+//     World.add(world, this.body)
+const makeBody = (x, y) => {
+  const body = Bodies.rectangle(0, 0, 40, 40)
+  World.add(world, body)
+  return body
+}
+class Box {
+  image
+  body
+
+  constructor(x, y) {
+    this.image = makeImage(x, y)
+    this.body = makeBody(x, y)
+  }
+  refresh() {
+    this.image.x++
+    console.log(this.image.x)
+    // this.image.y++
+    this.image.x = this.body.position.x
+    // console.log('image ' + this.image.x)
+    this.image.y = this.body.position.y
+  }
 }
 
 console.log(window.document.querySelector('canvas'))
